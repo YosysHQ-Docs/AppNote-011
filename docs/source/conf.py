fe436279859ed72efd3a5162d18fa25e21404d51
KrystalDelusion <93062060+KrystalDelusion@users.noreply.github.com>
2023-10-02 21:18:42 +1300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,11 +4,12 @@ author = 'YosysHQ GmbH'
 copyright ='2021 YosysHQ GmbH'
 
 # select HTML theme
-html_theme = 'press'
+html_theme = "furo"
+html_static_path = ['_static']
+
 html_logo = '../static/logo.png'
 html_favicon = '../static/favico.png'
 html_css_files = ['yosyshq.css', 'custom.css']
-html_sidebars = {'**': ['util/searchbox.html', 'util/sidetoc.html']}
 
 # These folders are copied to the documentation's HTML output
 html_static_path = ['../static', "../images"]
